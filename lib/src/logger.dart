4db90f9fbfdadba4ba38342b087db5edee45f9c4
Adam Stark <llamadonica@gmail.com>
2015-08-24 07:13:54 -0700

    A

--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -1,8 +1,13 @@
 library a_la_carte.server.logger;
 
+import 'dart:io';
+
 enum LoggerPriority {
   debug, info, message, warning, error, critical, fatal
 }
 
 typedef void Logger(String message, {LoggerPriority priority});
 
+void defaultLogger(String message, {LoggerPriority priority: LoggerPriority.info}) {
+  stderr.writeln('[$priority] $message');
+}
